beccddcc5e7d473c7e36efe1f023a6a026de2910
1q2w3e4r-kor <dldudco2@gmail.com>
2021-07-13 04:41:13 +0900

    Update Light_Control.ino
    라즈베리파이 MQTT 서버 이용

--- 아두이노_NODEMCU/Light_Control/Light_Control.ino
+++ 아두이노_NODEMCU/Light_Control/Light_Control.ino
@@ -1,14 +1,13 @@
-#include "AdafruitIO_WiFi.h"
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 #include <Adafruit_NeoPixel.h>
 
 // WiFI & adafruit_IO
-#define WIFI_SSID "ElijahL"
-#define WIFI_PASS "..."
-#define IO_USERNAME "whfwkr16"
-#define IO_KEY "..."
+#define WIFI_SSID ""
+#define WIFI_PASS ""
+#define MQTT_SERVER ""
 
-AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
-AdafruitIO_Feed *feed = io.feed("people", IO_USERNAME);
+
 
 // LED Pixel
 #define PERM_LED_PIN D2
@@ -19,6 +18,12 @@ AdafruitIO_Feed *feed = io.feed("people", IO_USERNAME);
 Adafruit_NeoPixel permStrip(LED_COUNT, PERM_LED_PIN, NEO_GRBW + NEO_KHZ800);
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRBW + NEO_KHZ800);
 
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+
+long last_send_time = 0; 
+char publish_msg[16];
 
 void setup() {
   Serial.begin(115200);
@@ -27,30 +32,79 @@ void setup() {
   strip.begin();
   permStrip.begin();
   initializeStrip();
+
+  //connect to wifi
+  setup_wifi();
+  client.setServer(MQTT_SERVER, 1883);
+  client.setCallback(callback);
   
-  // Connect to Adafruit IO
-  io.connect();
-  // wait for a connection
-  while(io.status() < AIO_CONNECTED) 
-  {
-    Serial.println(".");
-    delay(500);
-  }
-  Serial.println(io.statusText());
-  Serial.println("Connected");
-  feed->get();
 }
 
 void loop() {
-  io.run();
-  feed->onMessage(handleMessage);
+  //mqtt에서 계속 읽어오기
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  
 }
 
-void handleMessage(AdafruitIO_Data *data) {
-  Serial.print("people <-  ");
-  int people = data->toInt();
-  Serial.println(people);
-  stripOn(people);
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    stripOn(1);
+  } else {
+    stripOn(0);
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      client.subscribe("People");
+      Serial.println("subscribe People");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
+  
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void stripOn(int people){
