d8c39e219e57b421271cc5d891cc97b956281775
1q2w3e4r-kor <dldudco2@gmail.com>
2021-08-19 23:46:25 +0900

    DataBase Update

--- Database/Database.sql
+++ Database/Database.sql
@@ -52,6 +52,12 @@ CREATE TABLE Account (
  passwd varchar(20) NOT NULL
 );
 
+CREATE TABLE Notice (
+ datetime datetime NOT NULL,
+ store_id int NOT NULL,
+ contents varchar(40) NULL
+);
+
 ALTER TABLE Product ADD CONSTRAINT PK_PRODUCT PRIMARY KEY (
  id,
  plug_id,
@@ -95,22 +101,14 @@ ALTER TABLE Account ADD CONSTRAINT PK_ACCOUNT PRIMARY KEY (
  store_id
 );
 
-ALTER TABLE Product ADD CONSTRAINT FK_Smart_plug_TO_Product_1 FOREIGN KEY (
- plug_id
-)
-REFERENCES Smart_plug (
- id
+ALTER TABLE Notice ADD CONSTRAINT PK_NOTICE PRIMARY KEY (
+ datetime,
+ store_id
 );
+
 
 
 ALTER TABLE Env ADD CONSTRAINT FK_Store_TO_Env_1 FOREIGN KEY (
- store_id
-)
-REFERENCES Store (
- store_id
-);
-
-ALTER TABLE Smart_plug ADD CONSTRAINT FK_Store_TO_Smart_plug_1 FOREIGN KEY (
  store_id
 )
 REFERENCES Store (
@@ -143,6 +141,28 @@ ALTER TABLE Account ADD CONSTRAINT FK_Store_TO_Account_1 FOREIGN KEY (
 )
 REFERENCES Store (
  store_id
+);
+
+
+ALTER TABLE Notice ADD CONSTRAINT FK_Store_TO_Notice_1 FOREIGN KEY (
+ store_id
+)
+REFERENCES Store (
+ store_id
+);
+
+ALTER TABLE Smart_plug ADD CONSTRAINT FK_Store_TO_Smart_plug_1 FOREIGN KEY (
+ store_id
+)
+REFERENCES Store (
+ store_id
+);
+
+ALTER TABLE Product ADD CONSTRAINT FK_Smart_plug_TO_Product_1 FOREIGN KEY (
+ plug_id
+)
+REFERENCES Smart_plug (
+ id
 );
 
 ALTER TABLE Product ADD CONSTRAINT FK_Smart_plug_TO_Product_2 FOREIGN KEY (
